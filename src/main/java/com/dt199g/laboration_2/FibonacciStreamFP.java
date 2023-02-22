bed0f972889a94244a9a2b59d68e609cd635f578
Honorine Lima <holi1900@student.miun.se>
2023-02-22 10:09:24 +0100

    Complete FibonacciStreamFP and FibonacciStreamRX
    Add implementations for fibonacciVal, fibonacciSum and fibonacciSeqAndVal
    to both classes.
    fibonacciVal prints the fibonaccisequence
    fibonacciSum prints the of the fibonacci sequence
    fibonacciSeqAndVal prints the index and fibonacci sequence value as a pair

--- src/main/java/com/dt199g/laboration_2/FibonacciStreamFP.java
+++ src/main/java/com/dt199g/laboration_2/FibonacciStreamFP.java
@@ -14,21 +14,40 @@ public class FibonacciStreamFP implements FibonacciInterface {
      * {@inheritDoc}
      */
     @Override public void fibonacciVal(int limit) {
-
+        System.out.println(
+                Stream.iterate(new Pair(0, 1), pair -> new Pair(pair.second(), pair.first() + pair.second()))
+                        .limit(limit)
+                        .map(Pair::first)
+                        .map(l->new StringBuilder().append(l))
+                        .reduce(new StringBuilder(), (acc, s)->acc.append(", ").append(s))
+                        .delete(0,2)
+        );
     }
 
     /**
      * {@inheritDoc}
      */
     @Override public void fibonacciSum(int limit) {
-
+        System.out.println(
+                "Sum of " + limit + " sequential Fibonacci values: " + Stream.iterate(new Pair(0, 1), pair -> new Pair(pair.second(), pair.first() + pair.second()))
+                        .limit(limit)
+                        .map(Pair::first)
+                        .mapToInt(Long::intValue)
+                        .sum()
+        );
     }
 
     /**
      * {@inheritDoc}
      */
     @Override public void fibonacciSeqAndVal(int limit) {
-
+        System.out.println(
+                Stream.iterate(new int[]{0,0, 1}, val -> new int[]{val[0]+1, val[2], val[1] + val[2]})
+                        .limit(limit)
+                        .map(array -> new StringBuilder().append(array[0]).append(":").append(array[1]))
+                        .reduce(new StringBuilder(), (acc, s)->acc.append(" | ").append(s))
+                        .delete(0, 3)
+        );
     }
 
     /**
--- src/main/java/com/dt199g/laboration_2/FibonacciStreamRX.java
+++ src/main/java/com/dt199g/laboration_2/FibonacciStreamRX.java
@@ -2,6 +2,8 @@ package com.dt199g.laboration_2;
 
 import io.reactivex.rxjava3.core.Observable;
 
+import java.util.ArrayList;
+
 
 /**
  * ...
@@ -13,21 +15,37 @@ public class FibonacciStreamRX implements FibonacciInterface {
      * {@inheritDoc}
      */
     @Override public void fibonacciVal(int limit) {
-
+        Observable.range(1,limit-1)
+                //.repeat(limit)
+                .scan(new Pair(0, 1), (accumulator, val) -> new Pair(accumulator.second(), accumulator.first()+accumulator.second()))
+                .reduce(new StringBuilder(), (acc, s)->acc.append(", ").append(s.first()))
+                .map(s->s.delete(0, 2))
+                .subscribe(System.out::println);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override public void fibonacciSum(int limit) {
-
+        Observable.range(1,limit-1)
+                //.repeat(limit)
+                .scan(new Pair(0, 1), (accumulator, val) -> new Pair(accumulator.second(), accumulator.first()+accumulator.second()))
+                .reduce(0L, (acc, pair) ->acc + pair.first())
+                .subscribe(sum->System.out.printf("Sum of %d sequential Fibonnaci values: %,d%n", limit, sum));
     }
 
     /**
      * {@inheritDoc}
      */
     @Override public void fibonacciSeqAndVal(int limit) {
-
+        Observable.range(1,1)
+                .repeat(limit-1)
+                .scan(new Pair(0, 1), (accumulator, val) -> new Pair(accumulator.second(), accumulator.first()+accumulator.second()))
+                .map(Pair::first)
+                .zipWith(Observable.range(0, limit), (x,y) -> y + ":" + x)
+                .reduce(new StringBuilder(), (acc, s)->acc.append(" | ").append(s))
+                .map(s->s.delete(0, 3))
+                .subscribe(System.out::println);
     }
 
     /**
